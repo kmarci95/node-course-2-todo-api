f10ad1ae6eedad181aa741cf73a70a1b776c87d6
Kelemen Márton <kelemenmarton@Kelemen-MacBook-Pro.local>
2017-08-04 12:54:02 +0200

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -8,6 +8,8 @@ var {Todo} = require('./models/todo');
 
 var {User} = require('./models/user');
 
+const {ObjectID} = require('mongodb');
+
 var app = express();
 
 app.use(bodyParser.json());
@@ -32,6 +34,22 @@ app.get('/todos', (req, res) => {
         res.status(400).send(e);
     });
 });
+
+app.get('/todos/:id', (req, res) => {
+    var id = req.params.id;
+    if(!ObjectID.isValid(id)){
+        return res.status(404).send();  
+    }
+    Todo.findById(id).then((todo) => {
+        if(!todo){
+            return res.status(404).send();
+        }
+        res.send({todo});
+    }).catch((e) => {
+        res.status(400).send();
+    });
+});
+
 
 app.listen(3000, () => {
     console.log('started on port 3000');
