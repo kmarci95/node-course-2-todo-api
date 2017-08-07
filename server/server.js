cec810c651d34dff79e7c443496f4af5b716bea6
Kelemen Márton <kelemenmarton@Kelemen-MacBook-Pro.local>
2017-08-07 18:36:44 +0200

    Add Patch /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,7 @@
 var express = require('express');
 
+const _ = require ('lodash');
+
 var bodyParser = require('body-parser');
 
 var {mongoose} = require('./db/mongoose');
@@ -67,6 +69,28 @@ app.delete('/todos/:id', (req, res) => {
     });
 });
 
+app.patch('/todos/:id', (req, res) => {
+    var id = req.params.id;
+    var body = _.pick(req.body, ['text', 'completed']);
+    if(!ObjectID.isValid(id)){
+        return res.status(404).send();
+    }
+    if(_.isBoolean(body.completed) && body.completed){
+        body.completedAt = new Date().getTime();
+    }
+    else{
+        body.completed = false;
+        body.completedAt = null;
+    }
+    Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) => {
+        if(!todo){
+            return res.status(404).send();
+        }
+        res.send({todo});
+    }).catch((e) => {
+        res.status(400).send();
+    });
+});
 
 app.listen(port, () => {
     console.log(`Started up at port ${port}`);
